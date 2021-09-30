9617e6010863f3f4ecee27f7dc700c0ddc9c4086
Edward Vidal Jr <develone@sbcglobal.net>
2021-09-30 11:29:21 -0600

    testing ubuntu

--- sw/host/remrunjpeg.sh
+++ sw/host/remrunjpeg.sh
@@ -7,12 +7,12 @@
 ./pc-wbregs -n mypi3-19 0x01401018 0x1
 
 ./pc-zipload -n mypi3-19 -v ../board/jpeg
+./pc-wbregs -n mypi3-19 cpu 0x0f
 ./pc-wrsdram -n mypi3-19 b.bin
 ./pc-wbregs -n mypi3-19 0x01401008 0x0
 ./pc-wbregs -n mypi3-19 0x01401010 0x0
 #./pc-wrsdram rgb_pack.bin
-./pc-wbregs -n mypi3-19 cpu 0x0f
 #sleep 45
-#rm -f dwt.bin
-#./pc-rdsdram dwt.bin
+rm -f dwt.bin
+#./pc-rdsdram -n mypi3-19 dwt.bin
 
