dd94b86380711bd4200fb84f95258ef777e71faf
Edward Vidal Jr <develone@sbcglobal.net>
2021-10-04 16:20:46 -0600

    rem runkpeg.sh

--- sw/host/remrunjpeg.sh
+++ sw/host/remrunjpeg.sh
@@ -8,6 +8,9 @@
 
 ./pc-zipload -n mypi3-19 -v ../board/jpeg
 ./pc-wbregs -n mypi3-19 cpu 0x0f
+sleep 3
+./pc-wbregs -n mypi3-19 0x0140100c 
+sleep 3
 ./pc-wrsdram -n mypi3-19 b.bin
 ./pc-wbregs -n mypi3-19 0x01401008 0x0
 ./pc-wbregs -n mypi3-19 0x01401010 0x0
