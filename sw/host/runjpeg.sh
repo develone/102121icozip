b521fbe217992333c97de4cb186684d921a5c5b7
Edward Vidal Jr <develone@sbcglobal.net>
2024-01-09 06:18:14 -0700

    missing from last

--- sw/host/runjpeg.sh
+++ sw/host/runjpeg.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-./arm-wbregs 0x01401000 0x2
+./arm-wbregs 0x01401000 0x0
 ./arm-wbregs 0x01401004 0x1
-./arm-wbregs 0x01401008 0x1
+./arm-wbregs 0x01401008 0x0
 ./arm-wbregs 0x0140100c 0x0
-./arm-wbregs 0x01401010 0x1
-./arm-wbregs 0x01401018 0x1
+./arm-wbregs 0x01401010 0x0
+./arm-wbregs 0x01401018 0x0
 
 ./arm-zipload -v ../board/jpeg
-#./arm-wrsdram rgb_pack.bin
-./arm-wbregs cpu 0x0f
+#./arm-wrsdram r.bin
+#./arm-wbregs cpu 0x0f
 #sleep 45
 #rm -f dwt.bin
 #./arm-rdsdram dwt.bin
